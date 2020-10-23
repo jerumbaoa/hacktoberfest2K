6edcb3ef201738c6345d2072d8327a79dc51b9fa
Theressa Marie Tan <46438137+tmttan03@users.noreply.github.com>
2020-10-23 17:24:27 +0800

    Create tan-theressamarie.py

---
+++ scripts/tan-theressamarie.py
@@ -0,0 +1,9 @@
+import time
+
+def getFormattedDateTime ():
+	print ("returns a formatted and readable format for date and time")
+	return time.ctime()
+
+
+print (getFormattedDateTime())
+print (" ")
