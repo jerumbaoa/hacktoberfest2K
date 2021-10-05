8508c3549a3858a69cb744c850ad273b03a625b6
ESWAR CHAND <86471473+Chandu71202@users.noreply.github.com>
2021-10-05 20:14:13 +0530

    Create Eswar-Chand.py

---
+++ scripts/Eswar-Chand.py
@@ -0,0 +1,32 @@
+def merge(la,lb):
+    (C,m,n)=([],len(la),len(lb))
+    (i,j)=(0,0)
+    while ( i+j < m+n ):
+        if i==m :
+            #C.append(lb[j])
+            j=j+1
+        elif j==n :
+            C.append(la[i])
+            i=i+1
+        elif la[i]<lb[j]:
+            C.append(la[i])
+            i=i+1
+        elif la[i]>lb[j]:
+            #C.append(lb[j])
+            j=j+1
+        elif la[i]==lb[j]:
+            i=i+1
+            j=j+1
+    return C
+
+def mergesort(l,left,right):
+    if right-left <= 1 :
+        return(l[left:right])
+    if right-left > 1 :
+        mid=(right+left)//2
+        L=mergesort(l,left,mid)
+        R=mergesort(l,mid,right)
+        return merge(L,R)
+
+l=[10,10,12,14,9,8,13]
+print(mergesort(l,0,len(l)))
